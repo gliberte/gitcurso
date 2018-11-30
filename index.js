a51572eca7598ac360cbe3f92d4ac831217cf0f6
Luis Solano <luis.solano.l@gmail.com>
2018-11-30 11:24:03 -0500

    ok

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const http = require('http')
+const express = require('express')
+
+const app = express()
+
+
 class ProblemSolver{
-    
+    super()
+    solverOne(){
+        return 1+1
+    }
 }
+
+
+http.createServer(app).listen(5000,()=>console.log('server running on port 5000'))
+
